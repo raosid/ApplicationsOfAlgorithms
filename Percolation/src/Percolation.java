7ef9a9090047f457c08e17205bdc2556e42edb7d
raosid <sidgrao@gmail.com>
2014-12-15 23:04:58 -0800

    Created the first assignment

---
+++ Percolation/src/Percolation.java
@@ -0,0 +1,14 @@
+/**
+ * Created by sidrao on 12/15/14.
+ */
+public class Percolation {
+
+    private boolean[][] sites;
+
+    //Constructs a N-N grid. With all sites blocked.
+    public Percolation(int size) {
+        sites = new boolean[size][size];
+    }
+
+    
+}
